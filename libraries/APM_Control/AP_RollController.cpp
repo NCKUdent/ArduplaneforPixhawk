4b6731b50b6389d54f83feacd9bdcd188b3434f7
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2019-12-23 17:39:01 +0800

    Add files via upload

--- libraries/APM_Control/AP_RollController.cpp
+++ libraries/APM_Control/AP_RollController.cpp
@@ -267,7 +267,7 @@ int32_t AP_RollController::_custom_get_rate_out(float desired_rate, bool disable
 	_last_out_deg = ToDeg(_last_out);
 	
 	// Convert to centi-degrees and constrain, beware for physical system constraints
-	return constrain_float(_last_out_deg * 100, -165, 165);
+	return constrain_float(_last_out_deg * 100, -1000, 1000);
 }
 
 /*
