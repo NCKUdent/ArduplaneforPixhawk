d31982eb87d697bdafac5a17a669544c9205209e
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-03-18 20:37:53 +0800

    Create mode_lateral.cpp

---
+++ ArduPlane/mode_lateral.cpp
@@ -0,0 +1,50 @@
+#include "mode.h"
+#include "Plane.h"
+
+bool ModeLATERAL1::_enter()
+{
+    plane.throttle_allows_nudging = false;
+    plane.auto_throttle_mode = false;
+    plane.auto_navigation_mode = false;
+    plane.acro_state.locked_roll = false;
+    plane.acro_state.locked_pitch = false;
+
+
+    return true;
+}
+
+void ModeLATERAL1::update()
+{
+   
+	plane.nav_pitch_cd =0 ;
+
+	plane.count=plane.count+1;
+
+if (plane.count<30)
+{
+		
+	plane.steering_control.steering = plane.steering_control.rudder = 0;
+}
+else if (plane.count>31&&plane.count<60)
+{
+	plane.steering_control.steering = plane.steering_control.rudder = 1500;
+	
+			
+}
+else if (plane.count>61&&plane.count<80)
+{
+	plane.steering_control.steering = plane.steering_control.rudder = -1500;
+	
+}
+
+else if(plane.count>81&&plane.count<90)
+{
+	plane.steering_control.steering = plane.steering_control.rudder = 1500;
+	
+}
+
+else if(plane.count>91)
+{
+	plane.steering_control.steering = plane.steering_control.rudder = 0;
+	
+}
