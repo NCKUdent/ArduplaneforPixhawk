dfc855a077cf8579e1d341bbae755bfe37a1ef61
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-03-19 02:24:18 +0800

    Update AP_PitchController.cpp

--- libraries/APM_Control/AP_PitchController.cpp
+++ libraries/APM_Control/AP_PitchController.cpp
@@ -302,7 +302,7 @@ int32_t AP_PitchController::get_servo_out(int32_t angle_err, float scaler, bool 
 	// Pitch rate offset is the component of turn rate about the pitch axis
 	float aspeed;
 	float rate_offset;
-	bool inverted;
+	//bool inverted;
 
     if (gains.tau < 0.1f) {
         gains.tau.set(0.1f);
@@ -316,17 +316,17 @@ int32_t AP_PitchController::get_servo_out(int32_t angle_err, float scaler, bool 
 	// limit the maximum pitch rate demand. Don't apply when inverted
 	// as the rates will be tuned when upright, and it is common that
 	// much higher rates are needed inverted	
-	if (!inverted) {
-		if (_max_rate_neg && desired_rate < -_max_rate_neg) {
-			desired_rate = -_max_rate_neg;
-		} else if (gains.rmax && desired_rate > gains.rmax) {
-			desired_rate = gains.rmax;
-		}
-	}
+	//if (!inverted) {
+	//	if (_max_rate_neg && desired_rate < -_max_rate_neg) {
+	//		desired_rate = -_max_rate_neg;
+	//	} else if (gains.rmax && desired_rate > gains.rmax) {
+	//		desired_rate = gains.rmax;
+	//	}
+	//}
 	
-	if (inverted) {
-		desired_rate = -desired_rate;
-	}
+	//if (inverted) {
+	//	desired_rate = -desired_rate;
+	//}
 
 	// Apply the turn correction offset
 	desired_rate = desired_rate + rate_offset - rate_offset;
