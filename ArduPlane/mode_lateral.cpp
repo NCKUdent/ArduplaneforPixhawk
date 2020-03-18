c3dd781dd912b8ed9c51229b620e466898863da3
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-03-19 03:57:43 +0800

    Update mode_lateral.cpp

--- ArduPlane/mode_lateral.cpp
+++ ArduPlane/mode_lateral.cpp
@@ -19,6 +19,27 @@ void ModeLateral::update()
     plane.nav_pitch_cd =0 ;
 	plane.count=plane.count+1;
  
+if (plane.count<50)
+{
+		
+	plane.steering_control.steering = plane.steering_control.rudder = 0;
+}
+else if (plane.count<100)
+{
+	plane.steering_control.steering = plane.steering_control.rudder = 1500;
+	
+			
+}
+else if (plane.count<150)
+{
+	plane.steering_control.steering = plane.steering_control.rudder = -1500;
+	
+}
 
+else if (plane.count<200)
+{
+	plane.steering_control.steering = plane.steering_control.rudder = 0;
+	
+}
 
 }
