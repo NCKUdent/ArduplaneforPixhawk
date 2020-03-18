bf370a14ca30df9b77761a5c7fdd22099ff14bd3
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-03-18 20:35:29 +0800

    Update and rename mode_longitudinal to mode_longitudinal.cpp

--- ArduPlane/mode_longitudinal.cpp
+++ ArduPlane/mode_longitudinal.cpp
@@ -6,11 +6,16 @@ bool ModeLongitudinal::_enter()
     plane.throttle_allows_nudging = false;
     plane.auto_throttle_mode = false;
     plane.auto_navigation_mode = false;
+    
+    plane.acro_state.locked_roll = false;
+    plane.acro_state.locked_pitch = false;
 
     return true;
 }
 
 void ModeLongitudinal::update()
 {
+	plane.nav_roll_cd = 0;
 
+	plane.count=plane.count+1;
 }
