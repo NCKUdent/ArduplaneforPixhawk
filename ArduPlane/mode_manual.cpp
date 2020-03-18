3818d5469cc7350382716df777111243f5317e90
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-03-19 07:07:23 +0800

    Update mode_manual.cpp

--- ArduPlane/mode_manual.cpp
+++ ArduPlane/mode_manual.cpp
@@ -23,8 +23,11 @@ void ModeManual::update()
     SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, plane.channel_roll->get_control_in_zero_dz());
     SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, plane.channel_pitch->get_control_in_zero_dz());
     
-    plane.last_aileron = RC_Channel::rc_channel(1);
-    plane.last_elevator = RC_Channel::rc_channel(2);
+    //plane.last_aileron = RC_Channel::rc_channel(1);
+    //plane.last_elevator = RC_Channel::rc_channel(2);
+    
+    plane.last_aileron = plane.channel_roll->get_control_in_zero_dz();
+    plane.last_elevator = plane.channel_pitch->get_control_in_zero_dz();
     
     plane.id_input = plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz();
 }
