89461ad8bf0255888bf4b0be44c69a99f04ca54e
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-03-19 02:27:29 +0800

    Update AP_PitchController.cpp

--- libraries/APM_Control/AP_PitchController.cpp
+++ libraries/APM_Control/AP_PitchController.cpp
@@ -302,7 +302,7 @@ int32_t AP_PitchController::get_servo_out(int32_t angle_err, float scaler, bool 
 	// Pitch rate offset is the component of turn rate about the pitch axis
 	float aspeed;
 	float rate_offset;
-	//bool inverted;
+	bool inverted;
 
     if (gains.tau < 0.1f) {
         gains.tau.set(0.1f);
