844f735b7acbd8144b778794957c3804686cc945
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-04-17 21:30:31 +0800

    Update mode_lateral.cpp

--- ArduPlane/mode_lateral.cpp
+++ ArduPlane/mode_lateral.cpp
@@ -6,9 +6,6 @@ bool ModeLateral::_enter()
     plane.throttle_allows_nudging = false;
     plane.auto_throttle_mode = false;
     plane.auto_navigation_mode = false;
-    plane.acro_state.locked_roll = false;
-    plane.acro_state.locked_pitch = false;
-
 
     return true;
 }
