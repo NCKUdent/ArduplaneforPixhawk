2b1de9f8dfb30c41d31d5ab76bdbbc35f45e2ed2
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-04-17 19:00:56 +0800

    Update Attitude.cpp

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -512,6 +512,10 @@ void Plane::stabilize()
 		    //stabilize_yaw1(speed_scaler);
             stabilize_pitch(speed_scaler);
 		    lateral_input();
+                 if (g.stick_mixing == STICK_MIXING_DIRECT || control_mode == &mode_lateral) {
+                     stabilize_pitch(speed_scaler);
+                     stabilize_stick_mixing_direct();
+                     }
         //}
     }
 	if (control_mode == &mode_longitudinal)
