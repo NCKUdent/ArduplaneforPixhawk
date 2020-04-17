eac23f5fb2ba948896bb40b85d7288e81fdb01f3
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-04-18 02:24:58 +0800

    Update Attitude.cpp

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -507,9 +507,10 @@ void Plane::stabilize()
             //steer_state.locked_course = false;
             //steer_state.locked_course_err = 0;
             stabilize_pitch(speed_scaler);
-            lateral_input();
+            
             SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, plane.channel_roll->get_control_in_zero_dz());
             SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, plane.channel_pitch->get_control_in_zero_dz());
+            lateral_input();
             //stabilize_stick_mixing_direct();
             //stabilize_acro(speed_scaler);
 		    //stabilize_roll(speed_scaler);
