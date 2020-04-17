7c388ff48df01e5708ac5ec55b4475d87ec0a02f
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-04-17 18:13:08 +0800

    Update Attitude.cpp

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -411,7 +411,8 @@ if (plane.count<50)
     //SRV_Channels::set_output_pwm(SRV_Channel::k_aileron, RC_Channels::rc_channel(channel_roll)->get_radio_in());
     //SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, channel_roll->get_control_in_zero_dz());
     //SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, channel_roll->get_control_in());
-    channel_roll->get_control_in_zero_dz();
+    //channel_roll->get_control_in_zero_dz();
+    channel_roll->zero();
     }
     
 else if (plane.count<100)
