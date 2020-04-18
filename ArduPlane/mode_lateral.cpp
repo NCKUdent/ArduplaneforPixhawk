d4813419a6960659a227722a488187597e9cfc4a
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-04-18 17:08:46 +0800

    Update mode_lateral.cpp

--- ArduPlane/mode_lateral.cpp
+++ ArduPlane/mode_lateral.cpp
@@ -17,28 +17,28 @@ void ModeLateral::update()
     //SRV_Channels::set_output_scaled(SRV_Channel::k_rudder, plane.channel_rudder->get_control_in_zero_dz());
     plane.nav_pitch_cd =0 ;
 	plane.count=plane.count+1;
- /*
+ 
 if (plane.count<50)
 {
 		
-	plane.steering_control.steering = plane.steering_control.rudder = 0;
+	plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz() + 0;
 }
 else if (plane.count<100)
 {
-	plane.steering_control.steering = plane.steering_control.rudder = 1500;
+	plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz() + 1500;
 	
 			
 }
 else if (plane.count<150)
 {
-	plane.steering_control.steering = plane.steering_control.rudder = -1500;
+	plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz() - 1500;
 	
 }
 
 else if (plane.count<200)
 {
-	plane.steering_control.steering = plane.steering_control.rudder = 0;
+	plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz() + 0;
 	
 }
-*/
+
 }
