95be103411d705c37310f433ad99c29b45165b5e
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-04-18 22:31:09 +0800

    Update mode_lateral.cpp

--- ArduPlane/mode_lateral.cpp
+++ ArduPlane/mode_lateral.cpp
@@ -18,7 +18,7 @@ void ModeLateral::update()
     plane.nav_pitch_cd =0 ;
 	plane.count=plane.count+1;
  
-if (plane.channel_pitch->get_control_in() != 0 || plane.channel_roll->get_control_in() != 0) {
+if (plane.channel_pitch->percent_input() > 30 || plane.channel_roll->percent_input() > 30) {
         plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz();
 } else {
         if (plane.count<50) {
