03ba8f288e9883585e600892372e291561258635
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2020-05-14 02:27:44 +0800

    Update mode_lateral.cpp

--- ArduPlane/mode_lateral.cpp
+++ ArduPlane/mode_lateral.cpp
@@ -17,7 +17,7 @@ void ModeLateral::update()
     //SRV_Channels::set_output_scaled(SRV_Channel::k_rudder, plane.channel_rudder->get_control_in_zero_dz());
     plane.nav_pitch_cd =0 ;
 	plane.count=plane.count+1;
- 
+ /*
 if (plane.channel_pitch->percent_input() < 35 || plane.channel_pitch->percent_input() > 68 ||
     plane.channel_roll->percent_input() < 35  || plane.channel_roll->percent_input() > 68) {
         plane.count = 1000;
@@ -26,9 +26,9 @@ if (plane.channel_pitch->percent_input() < 35 || plane.channel_pitch->percent_in
         if (plane.count<50) {
             plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz() + 0;
         }
-        else if (plane.count<100) {
+        else if (plane.count<100) {*/
             plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz() + 1500;			
-        }
+        /*}
         else if (plane.count<150) {
             plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz() - 1500;
         }
@@ -38,5 +38,5 @@ if (plane.channel_pitch->percent_input() < 35 || plane.channel_pitch->percent_in
         else {
             plane.steering_control.steering = plane.steering_control.rudder = plane.channel_rudder->get_control_in_zero_dz();
         }
-    }
+    }*/
 }
